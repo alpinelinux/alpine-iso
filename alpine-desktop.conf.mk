cb4a0cf4e24305d61930f27c8a9a88362c420357
Natanael Copa <ncopa@alpinelinux.org>
2011-05-03 08:30:29 +0000

    use nenolods apkovl for desktop

--- alpine-desktop.conf.mk
+++ alpine-desktop.conf.mk
@@ -1,4 +1,4 @@
 ALPINE_NAME     := alpine-desktop
 MODLOOP_EXTRA   :=
 KERNEL_FLAVOR   := grsec
-APKOVL		:= http://dev.alpinelinux.org/~jbilyk/xfce.apkovl.tar.gz
+APKOVL		:= http://nenolod.net/~nenolod/localhost.apkovl.tar.gz
