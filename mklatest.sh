1b0095c65a88121dada5e66260f23c42cadda788
Natanael Copa <ncopa@alpinelinux.org>
2011-07-19 09:22:03 +0200

    include the sha1 sum in .latest.txt

--- mklatest.sh
+++ mklatest.sh
@@ -7,14 +7,16 @@ case "$(uname -m)" in
 	i[0-9]86) arch=x86;;
 esac
 
-
-
 current=$(cat current) || exit 1
 releasedir="v${current%.*}/releases/$arch"
 target=.latest.txt
 
 do_stat() {
-	stat -c "%y $releasedir/%n %s" *-$current-$arch.iso
+	for f in *-$current-$arch.iso; do
+		sha1=$(awk '{print $1}' $f.sha1)
+#		sha256=$(awk '{print $1}' $f.sha256)
+		stat -c "%y $releasedir/%n %s $sha1" $f
+	done
 }
 
 do_stat || exit 1
