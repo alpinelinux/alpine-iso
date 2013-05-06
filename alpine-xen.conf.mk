61a22aa626b6405d6d9ef1bcbf5afad1a4f9f080
Natanael Copa <ncopa@alpinelinux.org>
2013-05-06 14:12:30 +0000

    disable modeset for xen

--- alpine-xen.conf.mk
+++ alpine-xen.conf.mk
@@ -4,3 +4,4 @@ KERNEL_FLAVOR   := grsec
 # Example of serial console boot and Dom0 memory limit
 #XEN_PARAMS	:= com1=115200,8n1 console=com1 dom0_mem=512M
 #BOOT_OPTS	:= console=hvc0
+BOOT_OPTS	:= nomodeset
