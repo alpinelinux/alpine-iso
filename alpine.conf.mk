7db82b3d220d9554f01ab54d3cb8b539ae425c30
Natanael Copa <ncopa@alpinelinux.org>
2014-11-10 16:14:35 +0000

    alpine: remove flashcache

--- alpine.conf.mk
+++ alpine.conf.mk
@@ -1,4 +1,4 @@
 ALPINE_NAME     := alpine
 KERNEL_FLAVOR   := grsec
-MODLOOP_EXTRA	= $(addsuffix -grsec, dahdi-linux xtables-addons flashcache)
+MODLOOP_EXTRA	= $(addsuffix -grsec, dahdi-linux xtables-addons)
 
