0ba0fe5e8442aed2a2be977ee91c840fc1d00f00
Bartłomiej Piotrowski <b@bpiotrowski.pl>
2015-12-04 11:25:43 +0100

    Add f2fs to RPi initfs

--- alpine-rpi.conf.mk
+++ alpine-rpi.conf.mk
@@ -2,4 +2,4 @@ ALPINE_NAME     := alpine-rpi
 KERNEL_FLAVOR   := rpi rpi2
 MODLOOP_EXTRA   := 
 BOOT_OPTS	:= dwc_otg.lpm_enable=0 console=ttyAMA0,115200 console=tty1
-INITFS_FEATURES := base bootchart squashfs ext2 ext3 ext4 kms mmc raid scsi usb
+INITFS_FEATURES := base bootchart squashfs ext2 ext3 ext4 f2fs kms mmc raid scsi usb
