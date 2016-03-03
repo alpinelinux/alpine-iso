alpinelinux/alpine-iso

commit 93bdebeb7b8a89e6e991d13f27de38c4d7b83433

include only needed firmware files in the .iso modloop

Makefile
@@ -89,7 +89,12 @@ $(MODLOOP_KERNELSTAMP):
 		$(MODLOOP_PKGS) $(UBOOT_PKGS)
 	@mv "$(MODLOOP_DIR)"/tmp/lib/modules/* "$(MODLOOP_DIR)"/lib/modules/
 	@if [ -d "$(MODLOOP_DIR)"/tmp/lib/firmware ]; then \
-		mv "$(MODLOOP_DIR)"/tmp/lib/firmware "$(MODLOOP_DIR)"/lib/modules/;\
+		mkdir -p "$(MODLOOP_DIR)"/lib/firmware/; \
+		find "$(MODLOOP_DIR)"/lib/modules -type f -name "*.ko" | xargs modinfo -F firmware | sort -u | while read FW; do \
+			if [ -e "$(MODLOOP_DIR)/tmp/lib/firmware/$${FW}" ]; then \
+				install -pD "$(MODLOOP_DIR)/tmp/lib/firmware/$${FW}" "$(MODLOOP_DIR)/lib/firmware/$${FW}"; \
+			fi \
+		done \
 	fi
 	@cp $(MODLOOP_DIR)/tmp/usr/share/kernel/$*/kernel.release $@
 

commit b7caa30bb8addbaa6b84baf9ebf4e2a3e885da86

fix firmware location in modloop
modloop init.d script expects the firmware in /lib/modules/firmware.

This fixes regression introduced with commit 93bdebeb7b8a (include only
needed firmware files in the .iso modloop)

Makefile
@@ -89,10 +89,9 @@ $(MODLOOP_KERNELSTAMP):
 		$(MODLOOP_PKGS) $(UBOOT_PKGS)
 	@mv "$(MODLOOP_DIR)"/tmp/lib/modules/* "$(MODLOOP_DIR)"/lib/modules/
 	@if [ -d "$(MODLOOP_DIR)"/tmp/lib/firmware ]; then \
-		mkdir -p "$(MODLOOP_DIR)"/lib/firmware/; \
 		find "$(MODLOOP_DIR)"/lib/modules -type f -name "*.ko" | xargs modinfo -F firmware | sort -u | while read FW; do \
 			if [ -e "$(MODLOOP_DIR)/tmp/lib/firmware/$${FW}" ]; then \
-				install -pD "$(MODLOOP_DIR)/tmp/lib/firmware/$${FW}" "$(MODLOOP_DIR)/lib/firmware/$${FW}"; \
+				install -pD "$(MODLOOP_DIR)/tmp/lib/firmware/$${FW}" "$(MODLOOP_DIR)/lib/modules/firmware/$${FW}"; \
 			fi \
 		done \
 	fi