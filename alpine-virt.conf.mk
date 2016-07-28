260faac06f5e8233bb59876a33cdcb9d1d24313a
Natanael Copa <ncopa@alpinelinux.org>
2016-07-28 09:42:46 +0000

    alpine-virt: enable serial by default

--- alpine-virt.conf.mk
+++ alpine-virt.conf.mk
@@ -2,4 +2,5 @@ ALPINE_NAME     := alpine-virt
 MODLOOP_EXTRA   :=
 KERNEL_FLAVOR   := virtgrsec
 KERNEL_FLAVOR_DEFAULT := virtgrsec
-
+SYSLINUX_SERIAL	:= serial 0 115200
+BOOT_OPTS	:= console=tty0 console=ttyS0,115200
